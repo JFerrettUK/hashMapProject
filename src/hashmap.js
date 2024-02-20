9781207be71ec55a5d9d8c754aae070a26a39c8f
JFerrettUK <jferrettuk@gmail.com>
2024-02-20 09:08:33 +0000

    has, remove, length, clear, keys implemented

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,43 +1,108 @@
 function hashmap(key) {
-  let bucket = {};
+  let buckets = {};
   console.log("bucket below");
-  console.log(bucket);
+  console.log(buckets);
 
   function hash(key) {
     let hashCode = 0;
-
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % 10000;
     }
-
-    return hashCode;
+    return hashCode.toString();
   }
 
   function set(key, value) {
-    const hashedKey = hash(key).toString();
+    const hashedKey = hash(key);
+
+    if (!buckets[hashedKey]) {
+      buckets[hashedKey] = [];
+    }
 
     let found = false;
-
-    for (let i = 0; i < bucket[hashedKey].length; i++) {
-      if (bucket[hashedKey][i].key === key) {
-        bucket[hashedKey][i].value = value;
+    for (let i = 0; i < buckets[hashedKey].length; i++) {
+      if (buckets[hashedKey][i].key === key) {
+        buckets[hashedKey][i].value = value;
         found = true;
         break;
       }
     }
 
     if (!found) {
-      bucket[hashedKey].push({ key, value });
+      buckets[hashedKey].push({ key, value });
     }
   }
 
   function get(key) {
     const hashedKey = hash(key).toString();
-    return bucket[hashedKey];
+    if (buckets[hashedKey]) {
+      for (let i = 0; i < buckets[hashedKey].length; i++) {
+        if (buckets[hashedKey][i].key === key) {
+          return buckets[hashedKey][i].value;
+        }
+      }
+      return null;
+    } else {
+      return null;
+    }
   }
 
-  return { set, get, bucket };
+  function has(key) {
+    const hashedKey = hash(key).toString();
+    if (buckets[hashedKey]) {
+      for (let i = 0; i < buckets[hashedKey].length; i++) {
+        if (buckets[hashedKey][i].key === key) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      return false;
+    }
+  }
+
+  function remove(key) {
+    const hashedKey = hash(key).toString();
+    if (buckets[hashedKey]) {
+      for (let i = 0; i < buckets[hashedKey].length; i++) {
+        if (buckets[hashedKey][i].key === key) {
+          buckets[hashedKey].splice(i, 1);
+          return true;
+        }
+      }
+      return false;
+    } else {
+      return false;
+    }
+  }
+
+  function length() {
+    let count = 0;
+
+    Object.keys(buckets).forEach(function (bucketKey) {
+      count += buckets[bucketKey].length;
+    });
+
+    return count;
+  }
+
+  function clear() {
+    buckets = {};
+  }
+
+  function keys() {
+    let newArray = [];
+
+    for (const property in buckets) {
+      for (let i = 0; i < buckets[property].length; i++) {
+        newArray.push(buckets[property][i].key);
+      }
+    }
+
+    return newArray;
+  }
+
+  return { set, get, has, remove, length, clear, keys, buckets };
 }
 
 export default hashmap;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import hashmap from "./hashmap";
 
 hashmap();
+const map = hashmap();
+
+console.log("--- Testing set ---");
+map.set("name", "John");
+console.log(map.buckets);
+
+console.log("--- Testing get ---");
+console.log(map.get("name"));
+
+console.log("--- Testing has ---");
+console.log(map.has("name"));
+console.log(map.has("nonexistent"));
+
+console.log("--- Testing remove ---");
+map.remove("name");
+console.log(map.has("name"));
+
+console.log("--- Testing length ---");
+map.set("newKey", "newValue");
+console.log(map.length());
+
+console.log("--- Testing clear ---");
+map.clear();
+console.log(map.length());
